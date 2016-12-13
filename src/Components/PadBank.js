4d369eba3b9e4c264ac9780869f6aedaaa8ad10d
Sean Smith <sean.smith.2009@gmail.com>
2016-12-12 21:40:26 -0800

    volume control

--- src/Components/PadBank.js
+++ src/Components/PadBank.js
@@ -13,64 +13,55 @@ class PadBank extends React.Component {
 					clip={this.props.currentPadBank[0].url}
 					keyTrigger="Q"
 					keyCode={81} 
-					updateDisplay={this.props.updateDisplay}
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 				<DrumPad 
 					clipId={this.props.currentPadBank[1].id} 
 					clip={this.props.currentPadBank[1].url}
 					keyTrigger="W"
 					keyCode={87} 
-					updateDisplay={this.props.updateDisplay} 
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 				<DrumPad 
 					clipId={this.props.currentPadBank[2].id} 
 					clip={this.props.currentPadBank[2].url}
 					keyTrigger="E"
 					keyCode={69}
-					updateDisplay={this.props.updateDisplay} 
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 				<DrumPad 
 					clipId={this.props.currentPadBank[3].id} 
 					clip={this.props.currentPadBank[3].url}
 					keyTrigger="A" 
 					keyCode={65} 
-					updateDisplay={this.props.updateDisplay} 
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 				<DrumPad 
 					clipId={this.props.currentPadBank[4].id} 
 					clip={this.props.currentPadBank[4].url}
 					keyTrigger="S" 
 					keyCode={83} 
-					updateDisplay={this.props.updateDisplay} 
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 				<DrumPad 
 					clipId={this.props.currentPadBank[5].id} 
 					clip={this.props.currentPadBank[5].url}
 					keyTrigger="D"
 					keyCode={68} 
-					updateDisplay={this.props.updateDisplay} 
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 				<DrumPad 
 					clipId={this.props.currentPadBank[6].id} 
 					clip={this.props.currentPadBank[6].url}
 					keyTrigger="Z"
 					keyCode={90} 
-					updateDisplay={this.props.updateDisplay}
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay}/>
 				<DrumPad 
 					clipId={this.props.currentPadBank[7].id} 
 					clip={this.props.currentPadBank[7].url}
 					keyTrigger="X"
 					keyCode={88} 
-					updateDisplay={this.props.updateDisplay} 
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 				<DrumPad 
 					clipId={this.props.currentPadBank[8].id} 
 					clip={this.props.currentPadBank[8].url}
 					keyTrigger="C"
 					keyCode={67} 
-					updateDisplay={this.props.updateDisplay} 
-					clipVolume={this.props.clipVolume} />
+					updateDisplay={this.props.updateDisplay} />
 			</div>
 		)
 	}
